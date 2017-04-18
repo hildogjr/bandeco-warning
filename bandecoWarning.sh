2bf315573e761a49875d11a58614a7761bf60ded
Hildo Guillardi Júnior <hildogjr@users.noreply.github.com>
2017-04-18 14:31:15 -0300

    Update bandecoWarning.sh

--- bandecoWarning.sh
+++ bandecoWarning.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 # Auxiliary script to execute bandecoWarnin.py on Ubuntu. Used also to configure the cron jobs.
+# Written by Hildo Guillardi Júnior - FEEC - UNICAMP - 13/Apr/2017
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 case $1 in
 	--install|-i|--config|-c)
 		# Configure the crontab to execute the warning automatic.
-		addcronjob(){ ( crontab -l -u $USER 2>/dev/null | grep -v -F "$2" ; echo "$1 $2" ) | crontab  -u $USER -;}
-		addcronjob "0 11,17 * * 1-5" "bash '$DIR/bandecoWarning.sh'" # Lunch & dinner warning
+		modifycronjob(){ ( crontab -l -u $USER 2>/dev/null | grep -v -F "$2" ; echo "$1 $2" ) | crontab  -u $USER -;}
+		modifycronjob "0 11,17 * * 1-5" "bash '$DIR/bandecoWarning.sh'" # Lunch & dinner warning
 		#PATH=/home/anmol/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 		;;
+		
 	--uninstall|-u|--remove|-r)
 		# Remove tfrom the crontab.
 		removecronjob(){ ( crontab -l -u $USER 2>/dev/null | grep -v -F "$1" ) | crontab  -u $USER -;}
 		removecronjob "bash '$DIR/bandecoWarning'"
 		;;
+		
 	--direct|-d)
 		# Configure the crontab to execute the warning automatic, but calling the python script directly.
-		addcronjob(){ ( crontab -l -u $USER 2>/dev/null | grep -v -F "$2" ; echo "$1 $2" ) | crontab  -u $USER -;}
-		addcronjob "15 11,17 * * 1-5" "eval 'export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)'; DISPLAY=:0; python '$DIR/bandecoWarning.py'" # Lunch & dinner warning
+		modifycronjob(){ ( crontab -l -u $USER 2>/dev/null | grep -v -F "$2" ; echo "$1 $2" ) | crontab  -u $USER -;}
+		modifycronjob "15 11,17 * * 1-5" 'eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)";'" DISPLAY=:0; python '$DIR/bandecoWarning.py'" # Lunch & dinner warning
 		;;
+		
 	*)
 		# Just call the python function.
 	#	DBUS_SESSION_BUS_ADDRESS=unix:abstract=/tmp/dbus-PwezoBTpF3
